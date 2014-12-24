8ac03d79a90732210f373dad8065ed14cfb58fe1
Eduardo dos santos <rockero_spg@hotmail.com>
2014-12-24 19:39:44 -0200

    Criada as Tables view

---
+++ Aula04PUCTableView/Aula04PUCTableView/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  Aula04PUCTableView
+//
+//  Created by Eduardo dos santos on 24/12/14.
+//  Copyright (c) 2014 Eduardo dos santos. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
