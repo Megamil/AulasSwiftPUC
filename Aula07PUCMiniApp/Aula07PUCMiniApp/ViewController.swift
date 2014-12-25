64a5caee7a2943e29124b03db8681b8c75d73224
Eduardo dos santos <rockero_spg@hotmail.com>
2014-12-25 21:38:06 -0200

    Criado conteúdo na tabela

--- Aula07PUCMiniApp/Aula07PUCMiniApp/ViewController.swift
+++ Aula07PUCMiniApp/Aula07PUCMiniApp/ViewController.swift
@@ -8,18 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var imagesArray: [(name: String,image: String)] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagesArray = [(" WATCH","img1"),(" iOS 8","img2"),(" PAY","img3")]
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var tableViewcell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        
+        tableViewcell.textLabel?.text = imagesArray[indexPath.row].name
+        
+        return tableViewcell;
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imagesArray.count
+    }
 
 }
 
