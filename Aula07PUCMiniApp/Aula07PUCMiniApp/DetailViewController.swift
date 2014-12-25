4b07042ce7e1bd4f62e585697ce4ee56cef22703
Eduardo dos santos <rockero_spg@hotmail.com>
2014-12-25 21:43:48 -0200

    Criada nova View controller UI

---
+++ Aula07PUCMiniApp/Aula07PUCMiniApp/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  Aula07PUCMiniApp
+//
+//  Created by Eduardo dos santos on 25/12/14.
+//  Copyright (c) 2014 Eduardo dos santos. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
